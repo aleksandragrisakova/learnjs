2539768f01247ee35f86d7e03ab2ef34a95b12de
Aleksandra Grisakova <aleksandra.grisakova@tptlive.ee>
2025-09-30 09:27:22 +0300

    add types of example

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log('Hello World!');
+var num = 1; // dont dont dot
+let num2 = 2;
+console.log(num2, num);
+num = 3.5;
+console.log(typeof num);
+num = 3; 
+console.log(typeof num);
+let text = 'Aleksandra';
+console.log(typeof text);
+let bool = true;
+console.log(typeof bool);
+bool = false;
+console.log(typeof bool);
+let nothing;
+console.log(typeof nothing);
+nothing = undefined;
+nothing = null;
+console.log(typeof nothing);
+let array = [1, 2, 3, 4, 'sad', true, 3.5]
+console.log(typeof array); 
+let obj = {
+    name: 'Aleksandra',
+    age: 17,
+    cat: ['Fyrr'],
+    data: {
+        phone: '1234567',
+        email: 'aleksandra.grisakova@tptlive.ee'
+    }
+};
+console.log(typeof obj);
+function hello( ) {
+    console.log('hello');
+}
+
+console.log(typeof hello);
