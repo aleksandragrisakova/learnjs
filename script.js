906e70ef5d1f9659b9eb4eefe6751f9d5bacbe6f
Aleksandra Grisakova <aleksandra.grisakova@tptlive.ee>
2025-09-30 09:48:06 +0300

    add math examples

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-console.log('Hello World!');
-var num = 1; // dont dont dot
-let num2 = 2;
-console.log(num2, num);
-num = 3.5;
-console.log(typeof num);
-num = 3; 
-console.log(typeof num);
-let text = 'Aleksandra';
-console.log(typeof text);
-let bool = true;
-console.log(typeof bool);
-bool = false;
-console.log(typeof bool);
-let nothing;
-console.log(typeof nothing);
-nothing = undefined;
-nothing = null;
-console.log(typeof nothing);
-let array = [1, 2, 3, 4, 'sad', true, 3.5]
-console.log(typeof array); 
-let obj = {
-    name: 'Aleksandra',
-    age: 17,
-    cat: ['Fyrr'],
-    data: {
-        phone: '1234567',
-        email: 'aleksandra.grisakova@tptlive.ee'
-    }
-};
-console.log(typeof obj);
-function hello( ) {
-    console.log('hello');
-}
-
-console.log(typeof hello);
+console.log(1 + 2);
+let num = 1; 
+console.log(num);
+num = num + 2;
+console.log(num);
+num = num - 1;
+console.log(num);
+num = num * 2;
+console.log(num);
+num = num / 3;
+console.log(num);
+num = 3;
+num = num ** 3;
+console.log(num);
+num = 9;
+num = num % 2; // modulo
+console.log(num);
+num += 2;
+num -= 2;
+num *= 2;
+num /= 2;
+num **=2;
+num %=2; 
+console.log(num);
+num++;
+console.log(num);
+num--;
+console.log(num);
+num = Math.sqrt(9)
+console.log(num);
