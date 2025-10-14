ac963d27fb1da341c2c9520ed67998b963ec0633
Aleksandra Grisakova <aleksandra.grisakova@tptlive.ee>
2025-10-14 09:39:42 +0300

    add loop examples

--- script.js
+++ script.js
@@ -1,52 +1,44 @@
-let num = 11;
-if(num >= 10) {
-    console.log('Suurem kui kümme!');
-} else if (num === 10) {
-    console.log('Võrdne kümmega!');
-} else {
-    console.log('Väiksem kui kümme!');
+for(let i=0;i<10;i++){
+    console.log(i);
 }
 
-let day = new Date('2025-10-11').getDay();
-console.log(day);
-if(day === 1) {
-    console.log('Esmaspäev');
-} else if(day === 2) {
-    console.log('Teisipäev');
-} else if(day === 3) {  
-    console.log('Kolmapäev');
-} else if (day === 4) {
-    console.log('Neljapäev');
-} else if(day === 5 ||  day === 6) {
-    console.log('Reede');
-} else if(day === 6) {
-    console.log('Laupäev');
-} else if(day === 0) {
-        console.log('Pühapäev');
-} else {
-    console.log('imelikpäev');
+for(let i=9;i>=0;i--) {
+    console.log(i);
 }
 
-switch (day) {
-    case 1:
-        console.log('Esmaspäev');    
-        break;
-    case 2:
-        console.log('Teisipäev');    
-        break;
-    case 3:
-        console.log('Kolmpäev');    
-        break;
-    case 4:
-        console.log('Neljapäev');    
-        break;
-    case 5:
-    case 6:
-        console.log('Pidupäev');    
-        break;
-    case 0:
-        console.log('Pühapäev');    
-        break;
-    default:
-        console.log('Imelikpäev');         
+for(let i=1;i<1_000_000;i*=2) {
+    console.log(i);
 }
+
+for(let i=0;i>10;i++){
+    console.log(i);
+}
+
+// for(let i=0; i<10;i--){
+//    console.log(i);
+// }
+
+let time = Date.now();
+console.log(start);
+let i = 0;
+while(Date.now() <start+5000) {
+    i++;
+}
+console.log(i);
+
+//while(true) {
+
+// }
+
+let num = 5;
+
+while(num>5){
+    console.log('WHILE');
+}
+
+do {
+    console.log('DO');
+} while(num>5);
+
+
+
